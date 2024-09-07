126fa1d204ed582eed74377109408e3818023e4e
Toso <hello@iamtoso.com>
2024-09-07 13:59:08 +0300

    shadcn

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,7 +15,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <ClerkProvider
-    afterSignOutUrl={"/"}
+      afterSignOutUrl={"/"}
       appearance={{
         elements: {
           formButtonPrimary: "primary-gradient",
