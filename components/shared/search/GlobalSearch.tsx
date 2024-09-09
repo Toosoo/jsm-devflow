794e3e0d4fa4730180ec0b30e741ceb95ed8a516
Toso <hello@iamtoso.com>
2024-09-09 15:52:42 +0300

    navbar

--- components/shared/search/GlobalSearch.tsx
+++ components/shared/search/GlobalSearch.tsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import { Input } from "@/components/ui/input";
+import Image from "next/image";
+import React from "react";
 
 const GlobalSearch = () => {
   return (
-    <div>GlobalSearch</div>
-  )
-}
+    <div className="relative w-full max-w-[600px] max-lg:hidden">
+      <div className="background-light800_darkgradient grod relative flex min-h-[55px] items-center gap-1 rounded-xl px-4">
+        <Image
+          src="/assets/icons/search.svg"
+          alt="search"
+          width={24}
+          height={24}
+          className="cursor-pointer"
+        />
+        <Input
+          type="text"
+          placeholder="Search globally"
+          value=""
+          className="paragraph-regular placeholder background-light800_darkgradient shadow-none outline-none "
+        />
+      </div>
+    </div>
+  );
+};
 
-export default GlobalSearch
+export default GlobalSearch;
